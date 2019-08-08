590a44483905e20e50bf70f442a1593424119dc9
Vanessa <vtostado@wesleyan.edu>
2019-08-07 22:22:59 -0400

    resizable nav

--- public/js/scroll.js
+++
@@ -1,7 +0,0 @@
- function scrollto(div) {
- $('html,body').animate(
- {
-
-  scrollTop: $("."+div).offset().top
- },'slow');
-}
--- public/js/sidenav.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-   var elems = document.querySelectorAll('.sidenav-trigger');
-   var instances = M.Dropdown.init(elems, {alignment:'right'});
- });
